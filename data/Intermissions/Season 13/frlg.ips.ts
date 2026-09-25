12709b50ffd05b1f7778564cb3cd0c8f20f320e6
m4-used-rollout <dave.was.r@gmail.com>
2026-09-25 00:49:46 +0000

    Added frlg.ips

---
+++ data/Intermissions/Season 13/frlg.ips.ts
@@ -0,0 +1,69 @@
+/// <reference path="../../tpp-data.ts" />
+Intermissions.Runs.push(
+    {
+        RunName: "FRLG.IPS",
+        ColorPrimary: "#FFCA38",
+        ColorSecondary: "#449900",
+        StartDate: "2026-09-25T02:00:00Z",
+        Duration: "255d",
+        HostName: "",
+        //HostImage: "img/hosts/a2.png",
+        Region: "Kanto",
+        Generation: 3,
+        Pokedex: "National",
+        Events: [
+            {
+                Group: "Badges", Name: "Boulder Badge", Image: "img/badges/boulder.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Cascade Badge", Image: "img/badges/cascade.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Thunder Badge", Image: "img/badges/thunder.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Rainbow Badge", Image: "img/badges/rainbow.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Soul Badge", Image: "img/badges/soul.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Marsh Badge", Image: "img/badges/marsh.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Volcano Badge", Image: "img/badges/volcano.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Badges", Name: "Earth Badge", Image: "img/badges/earth.png", ImageSource: "http://bulbapedia.bulbagarden.net/wiki/Badge#Indigo_League",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Lorelei", Image: "img/trainers/firered/lorelei.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Bruno", Image: "img/trainers/firered/bruno.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Agatha", Image: "img/trainers/firered/agatha.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Elite Four", Name: "Lance", Image: "img/trainers/firered/lance.png",
+                Time: "", Attempts: 0
+            },
+            {
+                Group: "Champions", Name: "AMT", Image: "img/trainers/firered/blue.png",
+                Time: "", Attempts: 0
+            },
+        ]
+    }
+);
